e343f0f5e55060600c96f7606da90728547a48af
Christian7547 <eliasgonzalesec@gmail.com>
2023-09-10 23:33:52 -0400

    Se puede agregar nuevos modelos desde Views/Item/New y Views/Item/Edit

--- PomaBrothers_Frontend/Models/ItemModel.cs
+++ PomaBrothers_Frontend/Models/ItemModel.cs
@@ -1,14 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace PomaBrothers_Frontend.Models
+﻿namespace PomaBrothers_Frontend.Models
 {
     public class ItemModel
     {
         public int Id { get; set; }
         public string ModelName { get; set; }
         public string Marker { get; set; }
-        public int CapacityOrSize { get; set; }
-        public string MeasurementUnit { get; set; }
+        public int? CapacityOrSize { get; set; }
+        public string? MeasurementUnit { get; set; }
     }
 }
