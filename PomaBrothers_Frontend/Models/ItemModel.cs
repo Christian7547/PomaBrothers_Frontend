30bb428fb3cc47871aa2f0f5b650e8b15b036f56
KarenPoma <karendocu6707@gmail.com>
2023-10-03 11:42:47 -0400

    cambio2

--- PomaBrothers_Frontend/Models/ItemModel.cs
+++ PomaBrothers_Frontend/Models/ItemModel.cs
@@ -5,13 +5,8 @@
         public int Id { get; set; }
         public string ModelName { get; set; }
         public string Marker { get; set; }
-<<<<<<< HEAD
         public int CapacityOrSize { get; set; }
         public string MeasurementUnit { get; set; }
-=======
-        public int? CapacityOrSize { get; set; }
-        public string? MeasurementUnit { get; set; }
         public virtual ICollection<Section> Sections { get; set; } = new List<Section>();
->>>>>>> chris
     }
 }
