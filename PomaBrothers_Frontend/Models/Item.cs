c97ebfb0e80d10bd28f1dca404c769d1eb077364
Catiier <yerlin.n.r.vivanco@gmail.com>
2023-10-03 22:40:34 -0400

    actualizar frontend

--- PomaBrothers_Frontend/Models/Item.cs
+++ PomaBrothers_Frontend/Models/Item.cs
@@ -21,15 +21,22 @@
         public string TypeWarranty { get; set; }
 #pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
         public int CategoryId { get; set; }
-        public DateTime RegisterDate { get; set; }
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
         public int ModelId { get; set; }
+<<<<<<< HEAD
 #pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+=======
+        public string UrlImage { get; set; }
+>>>>>>> 9af98a667f3475b7f03afff6ab1d660f8d4cb12d
         public virtual Category Category { get; set; }
 #pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
 #pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
         public virtual ItemModel ItemModel { get; set; }
+<<<<<<< HEAD
 #pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
         public virtual ICollection<DeliveryDetail> DeliveryDetails { get; set; } = new List<DeliveryDetail>();
+=======
+>>>>>>> 9af98a667f3475b7f03afff6ab1d660f8d4cb12d
         public virtual ICollection<SaleDetail> SaleDetails { get; set; } = new List<SaleDetail>();
     }
 }
